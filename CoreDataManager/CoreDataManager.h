075f138fd0995a08f5d159c9b85d35d1c40ee36a
Stoyan Stoyanov <stoqn.stoqnov.93@gmail.com>
2018-10-13 20:54:04 +0100

    Initial Commit

---
+++ CoreDataManager/CoreDataManager.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataManager.h
+//  CoreDataManager
+//
+//  Created by Stoyan Stoyanov on 13/10/2018.
+//  Copyright © 2018 Stoyan Stoyanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreDataManager.
+FOUNDATION_EXPORT double CoreDataManagerVersionNumber;
+
+//! Project version string for CoreDataManager.
+FOUNDATION_EXPORT const unsigned char CoreDataManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataManager/PublicHeader.h>
+
+
